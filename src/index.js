02de31412ae21ba52165a5db6fbab5265fc6b475
benoit <benoit@assetik.com>
2021-12-14 10:00:32 +0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+//import React from "react"
+//import ReactDOM from "react-dom"
+//
+//const element = <h1>Hello from Create React App</h1>
+//
+//ReactDOM.render(element, document.getElementById("root"))
+
+
+import React from "react"
+import ReactDOM from "react-dom"
+//component file
+import TodoContainer from "./components/TodoContainer"
+
+ReactDOM.render(<TodoContainer />, document.getElementById("root"))
